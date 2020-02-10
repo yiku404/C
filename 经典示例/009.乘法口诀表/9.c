7c45478cbedcdd0d3ae2a23adca5ec04334d9f6a
R <60771991+yiku404@users.noreply.github.com>
2020-02-10 11:04:03 +0800

    Update 9.c

--- 经典示例/009.乘法口诀表/9.c
+++ 经典示例/009.乘法口诀表/9.c
@@ -5,12 +5,9 @@ int main(){
         // 将下面的for循环注释掉，就输出左下三角形
         for(n=1; n<=9-i; n++)
             printf("        ");
-        
         for(j=1;j<=i;j++)
-            printf("%d*%d=%2d  ",i,j,i*j);
-        
+            printf("%d*%d=%2d  ",i,j,i*j)；
         printf("\n");
     }
-
     return 0;
 }
